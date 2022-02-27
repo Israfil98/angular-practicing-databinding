1cf6e769b9fa77eab6c3dd67d9f1e5aa4939176d
Israfil98 <israfil.guseynov.98@inbox.ru>
2022-02-27 20:06:03 +0400

    complete task(basics-assignment-2)

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  userName = ''
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  onResetUserName() {
+    this.userName = ''
+  }
+}
